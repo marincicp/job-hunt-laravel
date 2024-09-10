ee789ccc9a024cec75192f31b8c5b3dde112ba14
Petar <marincicpetar@gmail.com>
2024-09-10 15:32:06 +0200

    added tag -mcf

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,9 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
     use HasFactory;
     // public $fillable = ["name"];
+
+
+
+    public function jobs(): BelongsToMany
+    {
+
+
+
+        return $this->belongsToMany(Job::class);
+    }
 }
